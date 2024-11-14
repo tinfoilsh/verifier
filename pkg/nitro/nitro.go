12d7bd2da1efa652cde1baddf62e83d3850a770b
Nate Sales <nate@natesales.net>
2024-11-14 11:42:48 -0500

    refactor: extract AWS root cert to file

--- pkg/nitro/nitro.go
+++ pkg/nitro/nitro.go
@@ -2,6 +2,7 @@ package nitro
 
 import (
 	"crypto/x509"
+	_ "embed"
 	"encoding/hex"
 	"encoding/pem"
 	"fmt"
@@ -12,21 +13,26 @@ import (
 	"github.com/tinfoilanalytics/verifier/pkg/models"
 )
 
-// https://aws-nitro-enclaves.amazonaws.com/AWS_NitroEnclaves_Root-G1.zip
-const AwsRootCert = `-----BEGIN CERTIFICATE-----
-MIICETCCAZagAwIBAgIRAPkxdWgbkK/hHUbMtOTn+FYwCgYIKoZIzj0EAwMwSTEL
-MAkGA1UEBhMCVVMxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMRswGQYD
-VQQDDBJhd3Mubml0cm8tZW5jbGF2ZXMwHhcNMTkxMDI4MTMyODA1WhcNNDkxMDI4
-MTQyODA1WjBJMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQW1hem9uMQwwCgYDVQQL
-DANBV1MxGzAZBgNVBAMMEmF3cy5uaXRyby1lbmNsYXZlczB2MBAGByqGSM49AgEG
-BSuBBAAiA2IABPwCVOumCMHzaHDimtqQvkY4MpJzbolL//Zy2YlES1BR5TSksfbb
-48C8WBoyt7F2Bw7eEtaaP+ohG2bnUs990d0JX28TcPQXCEPZ3BABIeTPYwEoCWZE
-h8l5YoQwTcU/9KNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUkCW1DdkF
-R+eWw5b6cp3PmanfS5YwDgYDVR0PAQH/BAQDAgGGMAoGCCqGSM49BAMDA2kAMGYC
-MQCjfy+Rocm9Xue4YnwWmNJVA44fA0P5W2OpYow9OYCVRaEevL8uO1XYru5xtMPW
-rfMCMQCi85sWBbJwKKXdS6BptQFuZbT73o/gBh1qUxl/nNr12UO8Yfwr6wPLb+6N
-IwLz3/Y=
------END CERTIFICATE-----`
+// wget https://aws-nitro-enclaves.amazonaws.com/AWS_NitroEnclaves_Root-G1.zip
+
+//go:embed aws-nitro-root-g1.pem
+var awsRootCertBytes []byte
+
+var rootCert *x509.Certificate
+
+func init() {
+	// Parse AWS root cert
+	b, _ := pem.Decode(awsRootCertBytes)
+	if b == nil {
+		panic("parsing root cert")
+	}
+
+	var err error
+	rootCert, err = x509.ParseCertificate(b.Bytes)
+	if err != nil {
+		panic("parsing root cert: " + err.Error())
+	}
+}
 
 // AttestationDocument represents an AWS Nitro Enclave attestation document in CBOR format
 type AttestationDocument struct {
@@ -66,16 +72,6 @@ func VerifyAttestation(attDocBytes []byte) (*models.Measurements, error) {
 	}
 	if err := msg.Verify(nil, verifier); err != nil {
 		return nil, fmt.Errorf("verifying signature: %w", err)
-	}
-
-	// Parse AWS root cert
-	b, _ := pem.Decode([]byte(AwsRootCert))
-	if b == nil {
-		return nil, fmt.Errorf("parsing root cert")
-	}
-	rootCert, err := x509.ParseCertificate(b.Bytes)
-	if err != nil {
-		return nil, fmt.Errorf("parsing root cert: %w", err)
 	}
 
 	roots := x509.NewCertPool()
