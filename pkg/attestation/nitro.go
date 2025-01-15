b5cba89b1c92ca82b369699144c744df9821efda
Nate Sales <nate@natesales.net>
2025-01-14 22:47:52 -0500

    feat: AWS nitro enclave measurement verifier

---
+++ pkg/attestation/nitro.go
@@ -0,0 +1,34 @@
+package attestation
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"github.com/blocky/nitrite"
+)
+
+var (
+	NitroEnclaveVerifierOpts = nitrite.VerifyOptions{}
+)
+
+// parseAWSNitroAttestation decodes a base64 encoded attestation document,
+// verifies it against the AWS root, and returns the inner measurements
+func parseAWSNitroAttestation(attestationDoc string) (*Measurement, error) {
+	attDocBytes, err := base64.StdEncoding.DecodeString(attestationDoc)
+	if err != nil {
+		return nil, err
+	}
+	attestedResult, err := nitrite.Verify(attDocBytes, NitroEnclaveVerifierOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	pcrs := attestedResult.Document.PCRs
+	return &Measurement{
+		Type: AWSNitroEnclaveV1,
+		Registers: []string{
+			hex.EncodeToString(pcrs[0]),
+			hex.EncodeToString(pcrs[1]),
+			hex.EncodeToString(pcrs[2]),
+		},
+	}, nil
+}
