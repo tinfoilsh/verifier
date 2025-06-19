be716a1a189af7e524606bb29d9395caf0d04c35
Jules Drean <drean@mit.edu>
2025-06-19 00:26:33 -0700

    Add minimal validation of attestation report

--- attestation/sev.go
+++ attestation/sev.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 
 	"github.com/google/go-sev-guest/abi"
+	"github.com/google/go-sev-guest/kds"
 	"github.com/google/go-sev-guest/proto/sevsnp"
+	"github.com/google/go-sev-guest/validate"
 	"github.com/google/go-sev-guest/verify"
 	"github.com/google/go-sev-guest/verify/trust"
 	"google.golang.org/protobuf/types/known/wrapperspb"
@@ -52,7 +54,7 @@ func verifySevAttestation(attestationDoc string) (*Verification, error) {
 		return nil, err
 	}
 
-	opts := verify.DefaultOptions()
+	opts := verify.DefaultOptions() // TODO: What are the default options here?
 	opts.Getter = &getter{}
 	opts.Product = &sevsnp.SevProduct{
 		Name:            sevsnp.SevProduct_SEV_PRODUCT_GENOA,
@@ -64,7 +66,61 @@ func verifySevAttestation(attestationDoc string) (*Verification, error) {
 		return nil, fmt.Errorf("failed to parse report: %v", err)
 	}
 
-	if err := verify.SnpReport(parsedReport, opts); err != nil {
+	attestation, err := verify.GetAttestationFromReport(parsedReport, opts)
+	if err != nil {
+		return nil, fmt.Errorf("could not recreate attestation from report: %w", err)
+	}
+
+	if err := verify.SnpAttestation(attestation, opts); err != nil {
+		return nil, err
+	}
+
+	mintcb := kds.TCBParts{
+		BlSpl:    0x7,
+		SnpSpl:   0xe,
+		UcodeSpl: 0x48,
+	}
+
+	valOpts := &validate.Options{
+		GuestPolicy: abi.SnpPolicy{
+			SMT:          true,
+			MigrateMA:    false,
+			Debug:        false,
+			SingleSocket: false,
+		},
+		MinimumGuestSvn: 0,
+		// ReportData TODO make sure we don't need a nonce thanks to public key in the attestation report.
+		// HostData
+		// ImageID
+		// FamilyID
+		// ReportID
+		// ReportIDMA
+		// Measurement is verified in latter steps
+		// ChipID
+		MinimumBuild:              2,
+		MinimumVersion:            uint16((1 << 8) | 55), // 1.55
+		MinimumTCB:                mintcb,
+		MinimumLaunchTCB:          mintcb,
+		PermitProvisionalFirmware: true,
+		PlatformInfo: &abi.SnpPlatformInfo{
+			SMTEnabled:                  true,
+			TSMEEnabled:                 false,
+			ECCEnabled:                  false,
+			RAPLDisabled:                false,
+			CiphertextHidingDRAMEnabled: false,
+			AliasCheckComplete:          false,
+		},
+		RequireAuthorKey: false,
+		VMPL:             nil,
+		RequireIDBlock:   false,
+		// TrustedAuthorKey
+		// TrustedAuthorKeyHashes
+		// TrustedIDKeys
+		// TrustedIDKeyHashes
+		// CertTableOptions
+	}
+
+	if err := validate.SnpAttestation(attestation, valOpts); err != nil {
 		return nil, err
 	}
 
