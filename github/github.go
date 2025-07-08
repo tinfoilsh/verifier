4eb863ae5d929aa406c74e6c913aabf7caebc9ab
Nate Sales <nate@natesales.net>
2025-07-08 14:54:57 -0400

    feat: add github.FetchLatest

--- github/github.go
+++ github/github.go
@@ -11,30 +11,9 @@ import (
 	"github.com/tinfoilsh/verifier/util"
 )
 
-// FetchLatestDigest gets the latest release and attestation digest of a repo
-func FetchLatestDigest(repo string) (string, error) {
-	url := "https://api-github-proxy.tinfoil.sh/repos/" + repo + "/releases/latest"
-	releaseResponse, err := util.Get(url)
-	if err != nil {
-		return "", err
-	}
-
-	var responseJSON struct {
-		TagName string `json:"tag_name"`
-		Body    string `json:"body"`
-	}
-	if err := json.Unmarshal(releaseResponse, &responseJSON); err != nil {
-		return "", err
-	}
-
-	// Backwards compatibility for old EIF releases
-	eifRegex := regexp.MustCompile(`EIF hash: ([a-fA-F0-9]{64})`)
-	matches := eifRegex.FindStringSubmatch(responseJSON.Body)
-	if len(matches) > 1 {
-		return matches[1], nil
-	}
-
-	url = fmt.Sprintf(`https://api-github-proxy.tinfoil.sh/%s/releases/download/%s/tinfoil.hash`, repo, responseJSON.TagName)
+// FetchDigest fetches the attestation digest for a given repo and tag
+func FetchDigest(repo, tag string) (string, error) {
+	url := fmt.Sprintf(`https://api-github-proxy.tinfoil.sh/%s/releases/download/%s/tinfoil.hash`, repo, tag)
 	digestResp, err := http.Get(url)
 	if err != nil {
 		return "", err
@@ -48,6 +27,46 @@ func FetchLatestDigest(repo string) (string, error) {
 	}
 
 	return strings.TrimSpace(string(digest)), nil
+}
+
+// FetchLatest gets the latest release, tag, and attestation digest of a repo
+func FetchLatest(repo string) (string, string, error) {
+	url := "https://api-github-proxy.tinfoil.sh/repos/" + repo + "/releases/latest"
+	releaseResponse, err := util.Get(url)
+	if err != nil {
+		return "", "", err
+	}
+
+	var responseJSON struct {
+		TagName string `json:"tag_name"`
+		Body    string `json:"body"`
+	}
+	if err := json.Unmarshal(releaseResponse, &responseJSON); err != nil {
+		return "", "", err
+	}
+
+	// Backwards compatibility for old EIF releases
+	eifRegex := regexp.MustCompile(`EIF hash: ([a-fA-F0-9]{64})`)
+	matches := eifRegex.FindStringSubmatch(responseJSON.Body)
+	if len(matches) > 1 {
+		return matches[1], responseJSON.TagName, nil
+	}
+
+	digest, err := FetchDigest(repo, responseJSON.TagName)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to fetch digest for %s@%s: %v", repo, responseJSON.TagName, err)
+	}
+	return digest, responseJSON.TagName, nil
+}
+
+// FetchLatestDigest gets the latest release and attestation digest of a repo
+func FetchLatestDigest(repo string) (string, error) {
+	release, _, err := FetchLatest(repo)
+	if err != nil {
+		return "", err
+	}
+
+	return release, nil
 }
 
 // FetchAttestationBundle fetches the sigstore bundle from a repo for a given repo and EIF hash
