474f225f07cf325b46f4de534629966e4b434788
Nate Sales <nate@natesales.net>
2025-10-17 13:41:18 -0400

    feat: router selection

---
+++ client/router.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+)
+
+var (
+	defaultRouterRepo = "tinfoilsh/confidential-model-router"
+	defaultRouterURL  = "https://atc.tinfoil.sh/routers"
+)
+
+type Router struct {
+	repo    string
+	routers []string
+}
+
+func NewRouter() *Router {
+	return &Router{
+		repo:    defaultRouterRepo,
+		routers: []string{},
+	}
+}
+
+func (r *Router) GetRouter() (string, error) {
+	resp, err := http.Get(defaultRouterURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var routers []string
+	if err := json.NewDecoder(resp.Body).Decode(&routers); err != nil {
+		return "", err
+	}
+
+	return routers[rand.Intn(len(routers))], nil
+}
+
+func (r *Router) Client() (*SecureClient, error) {
+	router, err := r.GetRouter()
+	if err != nil {
+		return nil, err
+	}
+	return NewSecureClient(router, r.repo), nil
+}
+
+// NewDefaultClient creates a new secure client using the default router
+func NewDefaultClient() (*SecureClient, error) {
+	router := NewRouter()
+	return router.Client()
+}
